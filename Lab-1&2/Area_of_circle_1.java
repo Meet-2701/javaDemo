1272274a6d4e3efbf0d140c38e8618523c04dcbb
Meet-2701 <meet.d.patel2701@gmail.com>
2024-06-14 12:28:05 +0530

    first_change

--- Lab-1&2/Area_of_circle_1.java
+++ Lab-1&2/Area_of_circle_1.java
@@ -4,8 +4,8 @@ public class Area_of_circle_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        double radius = sc.nextInt();
+        double r = sc.nextInt();
 
-        System.out.println("Area = " + (3.14*radius*radius));
+        System.out.println("Area = " + (3.14*r*r));
     }
 }
